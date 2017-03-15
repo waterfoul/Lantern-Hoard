da9fc5171f788adf961a9a72e91f38303114aa0d
Jason Lam <jaylam111@gmail.com>
2017-03-15 17:19:53 -0400

    basic lobby

---
+++ browser/components/Lobby/Room.jsx
@@ -0,0 +1,15 @@
+
+import React from 'react';
+
+export const Room = () => (
+	<div>
+		<div className="lobby-room">
+			<div >Room Name</div>
+			<div>Aaron Jason Jason Geoff</div>
+			<div className="btn-group">
+				<button className="btn btn-primary"> Join </button>
+				<button className="btn btn-secondary"> Watch </button>
+			</div>
+		</div>
+	</div>
+);
