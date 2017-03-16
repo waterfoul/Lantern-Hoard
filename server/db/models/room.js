9352f01e487dd9aba50b05175d241df02ec983a0
Jason Powell <jpdevelopment@icloud.com>
2017-03-16 18:16:01 -0400

    added room model

--- server/db/models/room.js
+++ server/db/models/room.js
@@ -1,17 +1,40 @@
-// name: string
-// associated with between 1 and 4 users
-// associated with 4 characters
-// white character association
-// yellow character association
-// green character association
-// blue character association
-// password
-// game state json
-   // Deck states
-   // Monster info
-      // Stats
-      // Name
-   // Active Monster cards
-   // Board positions
-   // Armor values and injuries
-   // Gear grid
+const Sequelize = require('sequelize');
+const db = require('../index.js');
+
+const Room = db.define('room', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false,
+		unique: true,
+		validate: {
+			notEmpty: true
+		},
+	},
+	password: {
+		type: Sequelize.STRING,
+	},
+	gameState: {
+		type: Sequelize.STRING
+	}
+});
+
+module.exports = Room;
+
+/*
+Associated with between 1 and 4 users
+Associated with 4 characters
+White character association
+Yellow character association
+Green character association
+Blue character association
+
+Game State JSON
+  - Deck states
+  - Monster info
+  - Stats
+  - Name
+  - Active Monster cards
+  - Board positions
+  - Armor values and injuries
+  - Gear grid
+*/
