b9c9e02c332e93cc3551a5598092262fc6818f4b
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-16 14:05:59 -0500

    Eslint fix

--- browser/components/Game/Board.jsx
+++ browser/components/Game/Board.jsx
@@ -16,38 +16,38 @@ export const Board = connect(
 )(
 	({ boardState }) => (
 		<div>
-			<img src="/static/board.jpg" className="game-main-image"/>
+			<img src="/static/board.jpg" className="game-main-image" />
 			<div id="game-board-grid">
 				<div className={[
 					'game-board-square',
 					'white',
 					'x-' + boardState.positions.white[0],
 					'y-' + boardState.positions.white[1]
-				].join(' ')}></div>
+				].join(' ')} />
 				<div className={[
 					'game-board-square',
 					'yellow',
 					'x-' + boardState.positions.yellow[0],
 					'y-' + boardState.positions.yellow[1]
-				].join(' ')}></div>
+				].join(' ')} />
 				<div className={[
 					'game-board-square',
 					'green',
 					'x-' + boardState.positions.green[0],
 					'y-' + boardState.positions.green[1]
-				].join(' ')}></div>
+				].join(' ')} />
 				<div className={[
 					'game-board-square',
 					'blue',
 					'x-' + boardState.positions.blue[0],
 					'y-' + boardState.positions.blue[1]
-				].join(' ')}></div>
+				].join(' ')} />
 				<div className={[
 					'game-board-square',
 					'monster',
 					'x-' + boardState.positions.monster[0],
 					'y-' + boardState.positions.monster[1]
-				].join(' ')}></div>
+				].join(' ')} />
 			</div>
 		</div>
 	)
