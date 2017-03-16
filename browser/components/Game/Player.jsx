7692e9fa72b8f2e357617de4ecf54b223de2824c
Jason Lam <jaylam111@gmail.com>
2017-03-16 17:18:31 -0400

    wrapped player cards

--- browser/components/Game/Player.jsx
+++ browser/components/Game/Player.jsx
@@ -14,20 +14,32 @@ export const Player = connect(
 	({ playerGearState }) => (
 		<div>
 			<div id="player-card-wrapper" className="container-fluid">
-				<img src="/static/p1-gear.jpg" id="player-card-image" className="container-fluid" />
-				<div id="player-gear-grid">
-					<div className={[
-						'player-gear-square',
-						'geared',
-						'x-' + playerGearState.initialGear.cloth[0],
-						'y-' + playerGearState.initialGear.cloth[1]
-					].join(' ')} />
-					<div className={[
-						'player-gear-square',
-						'geared',
-						'x-' + playerGearState.initialGear.foundingStone[0],
-						'y-' + playerGearState.initialGear.foundingStone[1]
-					].join(' ')} />
+				<div className="row no-gutters">
+					<div id="player-gear-board" className="col-md-2">
+						<img src="/static/gear-grid/tokens.jpg" id="player-gear-image" className="container-fluid" />
+					</div>
+					<div id="player-armor-gear-wrapper" className="container-fluid">
+						<div id="player-gear-board" className="col-md-10">
+							<img src="/static/gear-grid/armor.jpg" id="player-gear-image" className="container-fluid" />
+						</div>
+						<div id="player-gear-grid" className="col-md-10">
+							<img src="/static/gear-grid/gear.jpg" id="player-gear-image" className="container-fluid" />
+						</div> {/* */} {/*armor-gear-wrapper*/}
+						<div id="player-gear-grid">
+							<div className={[
+								'player-gear-square',
+								'geared',
+								'x-' + playerGearState.initialGear.cloth[0],
+								'y-' + playerGearState.initialGear.cloth[1]
+							].join(' ')} />
+							<div className={[
+								'player-gear-square',
+								'geared',
+								'x-' + playerGearState.initialGear.foundingStone[0],
+								'y-' + playerGearState.initialGear.foundingStone[1]
+							].join(' ')} />
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
