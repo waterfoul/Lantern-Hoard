0983e943e6bca239c30e71ea59ecd9bfd231cd82
Jason Lam <jaylam111@gmail.com>
2017-03-16 18:14:54 -0400

    working gear grid

--- browser/components/Game/Player.jsx
+++ browser/components/Game/Player.jsx
@@ -5,8 +5,8 @@ export const Player = connect(
 	() => ({
 		playerGearState: {
 			initialGear: {
-				cloth: [2, 2],
-				foundingStone: [3, 2]
+				cloth: [1, 1],
+				foundingStone: [2, 1]
 			}
 		}
 	})
@@ -15,30 +15,33 @@ export const Player = connect(
 		<div>
 			<div id="player-card-wrapper" className="container-fluid">
 				<div className="row no-gutters">
-					<div id="player-gear-board" className="col-md-2">
-						<img src="/static/gear-grid/tokens.jpg" id="player-gear-image" className="container-fluid" />
+					<div id="player-token-board" className="col-md-2">
+						<img src="/static/gear-grid/tokens.jpg" id="player-tokens-image" />
 					</div>
 					<div id="player-armor-gear-wrapper" className="container-fluid">
 						<div id="player-gear-board" className="col-md-10">
-							<img src="/static/gear-grid/armor.jpg" id="player-gear-image" className="container-fluid" />
+							<img src="/static/gear-grid/armor.jpg" id="player-armor-image" />
 						</div>
 						<div id="player-gear-grid" className="col-md-10">
-							<img src="/static/gear-grid/gear.jpg" id="player-gear-image" className="container-fluid" />
+							<div>
+								<img src="/static/gear-grid/gear.jpg" id="player-gear-image" />
+								<div id="player-gear-grid-items">
+									<div className={[
+										'player-gear-square',
+										'geared',
+										'x-' + playerGearState.initialGear.cloth[0],
+										'y-' + playerGearState.initialGear.cloth[1]
+									].join(' ')} />
+									<div className={[
+										'player-gear-square',
+										'geared',
+										'x-' + playerGearState.initialGear.foundingStone[0],
+										'y-' + playerGearState.initialGear.foundingStone[1]
+									].join(' ')} />
+								</div>
+							</div>
 						</div> {/* */} {/*armor-gear-wrapper*/}
-						<div id="player-gear-grid">
-							<div className={[
-								'player-gear-square',
-								'geared',
-								'x-' + playerGearState.initialGear.cloth[0],
-								'y-' + playerGearState.initialGear.cloth[1]
-							].join(' ')} />
-							<div className={[
-								'player-gear-square',
-								'geared',
-								'x-' + playerGearState.initialGear.foundingStone[0],
-								'y-' + playerGearState.initialGear.foundingStone[1]
-							].join(' ')} />
-						</div>
+
 					</div>
 				</div>
 			</div>
