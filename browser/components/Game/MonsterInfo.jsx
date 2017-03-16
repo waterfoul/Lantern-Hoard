35ef729268e4d605cf7f1cc276b3984c1e5dac1b
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-16 14:25:52 -0500

    Monster stats are in

--- browser/components/Game/MonsterInfo.jsx
+++ browser/components/Game/MonsterInfo.jsx
@@ -4,19 +4,30 @@ import {connect} from 'react-redux';
 export const MonsterInfo = connect(
 	() => ({
 		boardState: {
-			positions: {
-				white: [0, 3],
-				yellow: [5, 6],
-				green: [7, 7],
-				blue: [1, 1],
-				monster: [4, 4]
+			monsterStats: {
+				luck: Math.floor(Math.random() * 20),
+				accuracy: Math.floor(Math.random() * 20),
+				evasion: Math.floor(Math.random() * 20),
+				speed: Math.floor(Math.random() * 20),
+				damage: Math.floor(Math.random() * 20),
+				toughness: Math.floor(Math.random() * 20),
+				movement: Math.floor(Math.random() * 20)
 			}
 		}
 	})
 )(
 	({ boardState }) => (
-		<div>
+		<div id="game-board-monster">
 			<img src="/static/monster-info.jpg" className="game-main-image" />
+			<div id="game-board-monster-container">
+				<div className="game-board-monster-stats luck"><div>{boardState.monsterStats.luck}</div></div>
+				<div className="game-board-monster-stats accuracy"><div>{boardState.monsterStats.accuracy}</div></div>
+				<div className="game-board-monster-stats evasion"><div>{boardState.monsterStats.evasion}</div></div>
+				<div className="game-board-monster-stats speed"><div>{boardState.monsterStats.speed}</div></div>
+				<div className="game-board-monster-stats damage"><div>{boardState.monsterStats.damage}</div></div>
+				<div className="game-board-monster-stats toughness"><div>{boardState.monsterStats.toughness}</div></div>
+				<div className="game-board-monster-stats movement"><div>{boardState.monsterStats.movement}</div></div>
+			</div>
 		</div>
 	)
 );
