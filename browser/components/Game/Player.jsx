b8039a3f270c7eb1a55b6863a78f1530fe201a2c
Jason Lam <jaylam111@gmail.com>
2017-03-17 18:21:45 -0400

    completed armor values

--- browser/components/Game/Player.jsx
+++ browser/components/Game/Player.jsx
@@ -22,8 +22,21 @@ export const Player = connect(
 
 					<div id="player-armor-gear-wrapper" className="container-fluid">
 
-						<div id="player-gear-board" className="col-md-10">
+						<div id="player-armor-board" className="col-md-10">
 							<img src="/static/gear-grid/armor.jpg" id="player-armor-image" />
+							boardState.monsterStats.waist }
+							<div id="player-armor-board-container">
+								<div className="player-armor-board-values head"><div>1</div></div>
+								<div className="player-armor-board-values body"><div>2</div></div>
+								<div className="player-armor-board-values waist"><div>3</div></div>
+								<div className="player-armor-board-values hand"><div>4</div></div>
+								<div className="player-armor-board-values foot"><div>5</div></div>
+							</div>
+
+							<div id="player-armor-wound-container">
+								<div className="player-armor-wound-values head"><div>X</div></div>
+							</div>
+
 						</div>
 
 						<div id="player-gear-grid" className="col-md-10">
