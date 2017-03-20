86b5e37d8520fe9c1536a4bc310d5555856ea102
Jason Powell <jpdevelopment@icloud.com>
2017-03-20 16:32:19 -0400

    added locations

---
+++ browser/data/items/organGrinder.js
@@ -0,0 +1,42 @@
+export const organGrinder = {
+	'Fecal Salve': {
+		descriptors: ['item', 'balm', 'stinky'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, 'blue'],
+		characterModifiers: {},
+		img: '/static/items/fecal-salve.jpg'
+	},
+	'Monster Grease': {
+		descriptors: ['item', 'consumable', 'soluble', 'stinky'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, 'green'],
+		characterModifiers: {},
+		img: '/static/items/monster-grease.jpg'
+	},
+	'Dried Acanthus': {
+		descriptors: ['item', 'herb', 'consumable'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, null],
+		characterModifiers: {},
+		img: '/static/items/dried-acanthus.jpg'
+	},
+	'Lucky Charm': {
+		descriptors: ['item', 'jewelry'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, 'blue', null, 'blue'],
+		characterModifiers: {},
+		img: '/static/items/lucky-charm.jpg'
+	},
+	'Monster Tooth Necklance': {
+		descriptors: ['item', 'jewelry', 'bone'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, 'red', null, null],
+		characterModifiers: {},
+		img: '/static/items/monster-tooth-necklace.jpg'
+	}
+};
