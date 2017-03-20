bba9383837df902702c94fcb5b7be89da436e6f1
Jason Powell <jpdevelopment@icloud.com>
2017-03-20 12:51:17 -0400

    added skinnery items

---
+++ browser/data/items/skinnery.js
@@ -0,0 +1,75 @@
+export const skinnery = {
+	'Rawhide Headband': {
+		value: 1,
+		slot: ['head'],
+		descriptors: ['armor', 'set', 'rawhide'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, 'blue', null],
+		characterModifiers: {},
+		img: '/static/items/rawhide-headband'
+	},
+	'Rawhide Gloves': {
+		value: 1,
+		slot: ['arms'],
+		descriptors: ['armor', 'set', 'rawhide'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, 'red'],
+		characterModifiers: {},
+		img: '/static/items/rawhide-gloves'
+	},
+	'Rawhide Vest': {
+		value: 1,
+		slot: ['body'],
+		descriptors: ['armor', 'set', 'rawhide'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: ['blue', 'red', null, null],
+		characterModifiers: {},
+		img: '/static/items/rawhide-vest'
+	},
+	'Rawhide Pants': {
+		value: 1,
+		slot: ['waist'],
+		descriptors: ['armor', 'set', 'rawhide'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, null],
+		characterModifiers: {},
+		img: '/static/items/rawhide-pants'
+	},
+	'Rawhide Boots': {
+		value: 1,
+		slot: ['legs'],
+		descriptors: ['armor', 'set', 'rawhide'],
+		traits: [],
+		specialAbilities: {},
+		colorEdges: [null, null, null, null],
+		characterModifiers: {},
+		img: '/static/items/rawhide-boots'
+	},
+	'Bandages': {
+		descriptors: ['item'],
+		specialAbilities: {},
+		colorEdges: [null, null, 'green', 'blue'],
+		img: '/static/items/bandages'
+	},
+	'Rawhide Drum': {
+		descriptors: ['item', 'rawhide', 'instrument', 'noisy'],
+		specialAbilities: {},
+		colorEdges: [null, null, null, 'green'],
+		img: '/static/items/rawhide-drum'
+	},
+	'Rawhide Whip': {
+		descriptors: ['weapon', 'melee', 'whip', 'rawhide'],
+		dice: 3,
+		accuracy: 7,
+		strength: 1,
+		diceMods: {},
+		specialAbilities: {},
+		colorEdges: [null, null, null, null],
+		characterModifiers: {},
+		img: '/static/items/rawhide-whip'
+	}
+};
