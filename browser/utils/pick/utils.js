f067f25df1aa66f988a2c8d4cd6bf549d5487157
Jason Powell <jpdevelopment@icloud.com>
2017-03-23 15:44:48 -0400

    added functions to check monster FoV

--- browser/utils/pick/utils.js
+++ browser/utils/pick/utils.js
@@ -55,3 +55,26 @@ export const findClosestAndChoose = (distances, dispatch) => {
 		return chooseBetween(resultArr, dispatch);
 	}
 };
+
+export function checkFieldOfView(gameState, position ) {
+	const monsterPosition = gameState.positions.monster;
+	const monsterSize = gameState.monsterStats.size;
+
+	if (gameState.monsterDirection === 'S') {
+		const difference = position[0] - monsterPosition[0];
+		return position[1] !== (monsterPosition[1] + 1) || difference < 0 || difference >= monsterSize;
+	}
+	else if (gameState.monsterDirection === 'N') {
+		const difference = position[0] - monsterPosition[0];
+		console.log(position, monsterPosition, difference, position[1] !== (monsterPosition[1] - monsterSize), difference < 0, difference >= monsterSize );
+		return position[1] !== (monsterPosition[1] - monsterSize) || difference < 0 || difference >= monsterSize;
+	}
+	else if (gameState.monsterDirection === 'E') {
+		const difference = position[1] - monsterPosition[1];
+		return position[0] !== (monsterPosition[0] - 1) || difference < 0 || difference >= monsterSize;
+	}
+	else if (gameState.monsterDirection === 'W') {
+		const difference = position[1] - monsterPosition[1];
+		return position[0] !== (monsterPosition[0] + monsterSize) || difference < 0 || difference >= monsterSize;
+	}
+}
