939a27806022849bd7d0ef5c85b2de53e697ca0e
Jason Lam <jaylam111@gmail.com>
2017-03-24 13:21:13 -0400

    conditions should be fixed

---
+++ browser/reducers/gameState/ai.js
@@ -0,0 +1,21 @@
+import {drawAICardAction, woundAIAction, setAIDeckAction} from '../../../common/gameState/ai';
+import {shuffle} from '../../utils/shuffle';
+
+export const drawAICard = () => (
+	(dispatch, getState) => {
+		const {room} = getState();
+		if (room.gameState.ai.deck.length === 0) {
+			dispatch(setAIDeckAction(shuffle(room.gameState.ai.discard)));
+		}
+		dispatch(drawAICardAction());
+	}
+);
+export const woundAI = () => (
+	(dispatch, getState) => {
+		let {board} = getState();
+		if (board.gameState.ai.deck.length === 0) {
+			dispatch(setAIDeckAction(shuffle(board.gameState.ai.discard)));
+		}
+		dispatch(woundAIAction());
+	}
+);
