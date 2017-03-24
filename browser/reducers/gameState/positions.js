4db95624c288c15db9b33deb06b0e152b2069bb5
Aaron Aichlmayr <waterfoul@gmail.com>
2017-03-24 09:10:19 -0500

    Eslint fixes

--- browser/reducers/gameState/positions.js
+++ browser/reducers/gameState/positions.js
@@ -34,10 +34,10 @@ function squishChar(room, location, pos, char, players, dispatch, distance = 1) 
 		return true;
 	});
 
-	if(options.length === 0) {
+	if (options.length === 0) {
 		return squishChar(room, location, pos, distance); //Not an ideal way to handle this but it will be a rare case so...
-	} else if(options.length === 1) {
-		console.log('Moving', char, 'to', options[0])
+	} else if (options.length === 1) {
+		console.log('Moving', char, 'to', options[0]);
 		dispatch(knockDownCharacter(char));
 		dispatch(move(`player${char + 1}`, options[0]));
 	} else {
