ed3b3c7677c5c0995ce0c1e8f93f631b7d08fd1a
Aaron Aichlmayr <waterfoul@gmail.com>
2017-04-04 12:54:45 -0500

    Enabled max-params, fixed a bug with an edgecase which we don't test very often

--- browser/reducers/gameState/positions.js
+++ browser/reducers/gameState/positions.js
@@ -10,7 +10,7 @@ export const move = (token, location) => (
 	}
 );
 
-function squishChar(room, location, pos, char, players, dispatch, distance = 1) {
+function squishChar({ room, location, pos, char, players, dispatch }, distance = 1) {
 	const options = [
 		[pos[0], pos[1] + distance],
 		[pos[0], pos[1] - distance],
@@ -35,7 +35,7 @@ function squishChar(room, location, pos, char, players, dispatch, distance = 1) 
 	});
 
 	if (options.length === 0) {
-		return squishChar(room, location, pos, distance); //Not an ideal way to handle this but it will be a rare case so...
+		return squishChar({ room, location, pos }, distance + 1); //Not an ideal way to handle this but it will be a rare case so...
 	} else if (options.length === 1) {
 		dispatch(knockDownCharacter(char));
 		dispatch(move(`player${char + 1}`, options[0]));
@@ -60,7 +60,7 @@ function squishChars(room, location, dispatch) {
 			(diffX <= 0 && diffX > -1 * room.gameState.monsterStats.size) &&
 			(diffY <= 0 && diffY > -1 * room.gameState.monsterStats.size)
 		) {
-			squishChar(room, location, pos, i, players, dispatch);
+			squishChar({ room, location, pos, char: i, players, dispatch });
 		}
 	});
 
